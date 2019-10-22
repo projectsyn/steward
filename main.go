106cb99232ef3bd861d71fbd9499f2997e512486
Simon Rüegg <simon.ruegg@vshn.ch>
2019-10-22 15:01:23 +0200

    Cluster registration
    Initial setup of the cluster agent. Registers the cluster to the SYN API
    and receives information about the catalog Git repo.

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/sirupsen/logrus"
+
+	"git.vshn.net/syn/steward/pkg/agent"
+
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+func main() {
+	logrus.SetLevel(logrus.DebugLevel)
+	app := kingpin.New("steward", "Steward makes your Kubernetes cluster SYN managed. 🎉")
+	app.DefaultEnvars()
+	app.Version("v0.0.1")
+	ctx, cancel := context.WithCancel(context.Background())
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, syscall.SIGTERM)
+	go receiveSignal(signalCh, cancel)
+
+	agent := agent.Agent{}
+	app.Action(func(*kingpin.ParseContext) error {
+		return agent.Run(ctx)
+	})
+
+	app.Flag("api", "API URL to connect to").Required().URLVar(&agent.APIURL)
+	app.Flag("token", "Token to authenticate to the API").Required().StringVar(&agent.Token)
+	app.Flag("cloud", "Cloud type this cluster is running on").StringVar(&agent.CloudType)
+	app.Flag("region", "Cloud region this cluster is running in").StringVar(&agent.CloudRegion)
+	app.Flag("distribution", "Kubernetes distribution this cluster is running").StringVar(&agent.Distribution)
+
+	kingpin.MustParse(app.Parse(os.Args[1:]))
+}
+
+func receiveSignal(signalCh chan os.Signal, cancel context.CancelFunc) {
+	for {
+		select {
+		case sig := <-signalCh:
+			logrus.Debugf("Received signal '%v'", sig)
+			cancel()
+		}
+	}
+}
