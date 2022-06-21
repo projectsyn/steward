be43d5b1ee7c87a15c965fcc5b8ca7360a551c6d
Sebastian Widmer <sebastian.widmer@vshn.net>
2022-06-21 15:21:23 +0200

    Renovate default images

---
+++ images.go
@@ -0,0 +1,11 @@
+package main
+
+// WARNING: Renovate updates the images in this file. If adding changes double check the
+// renovate.json file and it's regexManagers.
+
+const (
+	// DefaultArgoCDImage is the default image to use for the ArgoCD deployment.
+	// You should also update the CRDs in the manifests/ directory to match this version.
+	DefaultArgoCDImage = "quay.io/argoproj/argocd:v2.3.4"
+	DefaultRedisImage  = "docker.io/redis:6.2.4"
+)
